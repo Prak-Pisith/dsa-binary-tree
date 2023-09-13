a2dedb186100301da5f3e0f0c491370160adab65
prakp <prak.p@netobjex.com>
2023-09-13 23:46:44 +0700

    Implemented Simple manual binary tree

---
+++ manual-tree.js
@@ -0,0 +1,25 @@
+// Tree Node Class 
+var TreeNode = /** @class */ (function () {
+    function TreeNode(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    return TreeNode;
+}());
+// Initial Nodes
+var a = new TreeNode('a');
+var b = new TreeNode('b');
+var c = new TreeNode('c');
+var d = new TreeNode('d');
+var e = new TreeNode('e');
+var f = new TreeNode('f');
+// Connect Nodes
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ manual-tree.ts
@@ -0,0 +1,35 @@
+
+// Tree Node Class 
+class TreeNode<T> {
+  value: T;
+  left: TreeNode<T> | null;
+  right: TreeNode<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+}
+
+// Initial Nodes
+const a = new TreeNode('a');
+const b = new TreeNode('b');
+const c = new TreeNode('c');
+const d = new TreeNode('d');
+const e = new TreeNode('e');
+const f = new TreeNode('f');
+
+// Connect Nodes
+a.left = b;
+a.right = c;
+
+b.left = d;
+b.right = e;
+
+c.right = f;
+
+console.log(a);
+console.log(b);
+console.log(c);
